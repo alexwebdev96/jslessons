216ce5cf3896e5c504aa688e663a9d04d011eb0b
Oleksandr <strong.lebed22@gmail.com>
2023-02-19 18:02:06 +0200

    initiated console application

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 "use strict";
 
-const homeLink = 'someurl.com';
-const category = 'toys';
+const numberOfFilms = +prompt('Скільки фільмів ви переглянули?', '');
 
-console.log(`https://${homeLink}/${category}/4`);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+
+  },
+  actors: {
+
+  },
+  genres: [],
+  privat: false,
+};
+
+const a = prompt('Один з останніх фільмів?', '');
+const b = prompt('На скільки ви оцінюєте його?', '');
+const c = prompt('Один з останніх фільмів?', '');
+const d = prompt('На скільки ви оцінюєте його?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
